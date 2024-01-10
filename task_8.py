bdbeeb00f248b2468fc035a64cc2f77c3e3ea4af
Dmitriy Trifonov <dmitriy.trifonov@vk.team>
2024-01-10 18:30:38 +0300

    change task 8

--- task_8.py
+++ task_8.py
@@ -11,9 +11,11 @@ def print_all_files(dir_name):
 
 def print_all_files_recursive(dir_name, home_dir_name):
     qualified_item_names = (os.path.join(dir_name, filename) for filename in os.listdir(dir_name))
+
     for item in qualified_item_names:
         if os.path.isfile(item):
             print(os.path.relpath(os.path.join(dir_name, item), home_dir_name))
+
         elif os.path.isdir(item):
             new_dir_name = os.path.join(dir_name, item)
             print_all_files_recursive(new_dir_name, home_dir_name)
