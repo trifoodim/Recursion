bdad680ee41d3e61820880cf1a642d7e0389ba80
Dmitriy Trifonov <dmitriy.trifonov@vk.team>
2024-01-12 19:18:37 +0300

    change tasks 7, 8

--- task_7.py
+++ task_7.py
@@ -4,30 +4,24 @@
 # После того как вы отсортировали массив [2,5,4,3,5] по убыванию [5,5,4,3,2] берёте второй элемент 5, это и будет второе макс. Если массив [2,3,5,4], то второе макс будет 4, и т. д.
 
 
-def find_second_max_num(numbers):
+from typing import List, Optional
+
+
+def find_second_max_num(numbers: List) -> Optional[int]:
     if not numbers:
         return None
 
     max_num = numbers[0]
     second_max_num = None
-    i = 1
-
-    if i == len(numbers):
-        return None
-
-    if numbers[i] > max_num:
-        second_max_num = max_num
-        max_num = numbers[i]
-
-    if numbers[i] < max_num:
-        second_max_num = numbers[i]
-
-    idx = i + 1
+    idx = 1
 
     return find_second_max_num_recursive(idx, numbers, max_num, second_max_num)
 
 
-def find_second_max_num_recursive(idx, numbers, max_num, second_max_num):
+def find_second_max_num_recursive(idx: int,
+                                  numbers: List,
+                                  max_num: int,
+                                  second_max_num: int):
     if idx >= len(numbers):
         return second_max_num
 
--- task_8.py
+++ task_8.py
@@ -3,22 +3,26 @@
 
 
 import os
+from typing import List
 
 
-def print_all_files(dir_name):
-    print_all_files_recursive(dir_name, dir_name)
+def print_all_files(dir_name: str) -> List:
+    return print_all_files_recursive(dir_name, dir_name)
 
 
-def print_all_files_recursive(dir_name, home_dir_name):
+def print_all_files_recursive(dir_name: str, home_dir_name: str) -> List:
     qualified_item_names = (os.path.join(dir_name, filename) for filename in os.listdir(dir_name))
 
-    files_in_curr_dir = []
+    files_in_dir = []
     for item in qualified_item_names:
         if os.path.isfile(item):
-            files_in_curr_dir.append(os.path.relpath(os.path.join(dir_name, item), home_dir_name))
+            files_in_dir.append(os.path.relpath(os.path.join(dir_name, item), home_dir_name))
 
         elif os.path.isdir(item):
             new_dir_name = os.path.join(dir_name, item)
             print_all_files_recursive(new_dir_name, home_dir_name)
 
-    print(files_in_curr_dir)
+    return files_in_dir
+
+
+print(print_all_files('/Users/dmitriy.trifonov/Desktop'))
