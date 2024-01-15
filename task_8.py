e9205beaa207c16b0721f4872e8bf883c3eb9f84
Dmitriy Trifonov <dmitriy.trifonov@vk.team>
2024-01-15 11:16:07 +0300

    change task 8

--- task_8.py
+++ task_8.py
@@ -9,23 +9,15 @@ from typing import List
 import os
 
 
-def print_all_files(dir_name: str) -> List:
-    files_in_dir = []
-    print_all_files_recursive(dir_name, dir_name, files_in_dir)
-
-    return files_in_dir
-
-
-def print_all_files_recursive(dir_name: str,
-                              home_dir_name: str,
-                              files_in_dir: List) -> None:
+def print_all_files_recursive_in_one_function(dir_name: str) -> None:
 
     qualified_item_names = (os.path.join(dir_name, filename) for filename in os.listdir(dir_name))
+
     for item in qualified_item_names:
 
         if os.path.isfile(item):
-            files_in_dir.append(os.path.relpath(os.path.join(dir_name, item), home_dir_name))
+            print(os.path.join(dir_name, item))
 
         elif os.path.isdir(item):
             new_dir_name = os.path.join(dir_name, item)
-            print_all_files_recursive(new_dir_name, home_dir_name, files_in_dir)
+            print_all_files_recursive_in_one_function(new_dir_name)
